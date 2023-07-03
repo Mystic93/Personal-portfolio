d4573d06f12be29299c74ea77ebfaed1cfe59d22
Pol <polgil24@hotmail.com>
2023-07-03 18:54:09 +0200

    Add GlobalStyles

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,68 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+
+  *,
+  ::before,
+  ::after {
+    box-sizing: border-box;
+  }
+
+  html {
+      font-family: inherit
+  }
+
+  body {
+    margin: 0;
+    
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    margin: 0;
+    font : inherit;
+  }
+
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+
+  li {
+    list-style: none;
+  }
+
+  input {
+    font-family: inherit;
+    border: none;
+  }
+
+  button {
+    font: inherit;
+    border: none;
+    background-color: transparent;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  p {
+    margin: 0;
+  }
+
+  img{
+    max-width: 100%;
+    padding: 0;
+  }
+
+
+`;
+
+export default GlobalStyle;
