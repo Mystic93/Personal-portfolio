e5d3e78332b5c4b7f11116b24eae658020780d20
Pol <polgil24@hotmail.com>
2023-07-13 20:53:37 +0200

    Add Contact Component initial structure

---
+++ src/components/Contact/ContactStyled.tsx
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+
+export const ContactStyled = styled.section`
+  background-color: white;
+  min-height: 100vh;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  padding: 0px 30px;
+  align-items: center;
+
+  .contact-container__title {
+    display: flex;
+    justify-content: center;
+    color: ${(props) => props.theme.colors.aboutBlue};
+    text-align: center;
+    font-size: 1.8rem;
+    font-weight: 500;
+    padding: 20px;
+  }
+
+  .contact-article {
+    font-weight: 400;
+    font-size: 1.4rem;
+    text-align: center;
+
+    &__presentation {
+      padding-bottom: 30px;
+      line-height: 2;
+    }
+
+    &__logo {
+    }
+
+    &__button {
+      background-color: ${(props) => props.theme.colors.yellow};
+      border-radius: 15%;
+      padding: 5px;
+      padding-left: 15px;
+      padding-right: 15px;
+      display: flex;
+      margin: 0 auto;
+      margin-bottom: 30px;
+    }
+  }
+
+  .closing-info {
+    text-align: center;
+    font-size: 0.7rem;
+  }
+
+  span {
+    color: ${(props) => props.theme.colors.yellow};
+  }
+`;
