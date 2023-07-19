74327557977337256b65db4978a88a43b77c2f2b
Pol <polgil24@hotmail.com>
2023-07-20 01:15:28 +0200

    Add scroll to top Button

--- src/components/Main/MainStyled.tsx
+++ src/components/Main/MainStyled.tsx
@@ -1,8 +1,45 @@
-import { styled } from "styled-components";
+import { styled, keyframes } from "styled-components";
+
+const zoomInOutAnimation = keyframes`
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(2.1);
+  }
+  100% {
+    transform: scale(1);
+  }
+`;
 
 export const MainStyled = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   min-height: 100vh;
+
+  .scroll-button {
+    display: flex;
+    position: absolute;
+    bottom: 30px;
+    right: 30px;
+    z-index: 20;
+    width: 30px;
+    transition: all 0.4s;
+
+    cursor: pointer;
+  }
+  .arrow-up {
+    opacity: 0;
+    transition: all 0.4s;
+  }
+
+  img.scrolled {
+    opacity: 100;
+    transition: all 0.4s;
+    animation: ${zoomInOutAnimation} 1s ease 0s 1 normal none running;
+    display: flex;
+    position: fixed;
+    bottom: 30px;
+  }
 `;
